72238f2c344518f0c6e846122b71c7a91f00d013
jsantos-unica <48691804+jsantos-unica@users.noreply.github.com>
2019-03-19 16:10:19 -0300

    Update ebs-snapshot.sh

--- ebs-snapshot.sh
+++ ebs-snapshot.sh
@@ -20,9 +20,6 @@ logfile_max_lines="5000"
 retention_days="7"
 retention_date_in_seconds=$(date +%s --date "$retention_days days ago")
 
-alias aws=''$(which aws)' --output text --region us-east-1a'
-shopt -s expand_aliases
-
 ## Function Declarations ##
 
 # Function: Setup logfile and redirect stdout/stderr.
@@ -89,49 +86,8 @@ cleanup_snapshots() {
 			fi
 		done
 	done
-}
+}	
 
-# createAMI() {
-#         #To create a unique AMI name for this script
-#         INST_NAME="$(aws ec2 describe-instances --filters Name=instance-id,Values=$instance_id --query 'Reservations[*].Instances[*].Tags[?Key==`Name`].Value')"
-#         INST_TAG="$INST_NAME"_"$(date +%d%b%y)"
-#         echo -e "Starting the Daily AMI creation: $INST_TAG\n"
-
-#         #To create AMI of defined instance
-#         AMI_ID=$(aws ec2 create-image --instance-id "$instance_id" --name "$INST_TAG" --description "$instance_id"_"$(date +%d%b%y)" --no-reboot)
-#         echo "New AMI Id is: $AMI_ID"
-#         echo "Waiting for 0.5 minutes"
-#         sleep 30
-
-#         #Renaming AMI and its Snapshots
-#         aws ec2 create-tags --resources "$AMI_ID" --tags Key=Name,Value="$INST_TAG"
-#         aws ec2 describe-images --image-id "$AMI_ID" --query 'Images[*].BlockDeviceMappings[*].Ebs.SnapshotId' | tr -s '\t' '\n' > /tmp/newsnaplist.txt
-#         while read SNAP_ID; do
-#                 aws ec2 create-tags --resources "$SNAP_ID" --tags Key=Name,Value="$INST_TAG"
-#         done < /tmp/newsnaplist.txt
-
-#         #Finding AMI older than n which needed to be removed
-#         if [[ $(aws ec2 describe-images --filters Name=description,Values="$instance_id"_"$(date +%d%b%y --date ''$retention_days' days ago')" --query 'Images[*].BlockDeviceMappings[*].Ebs.SnapshotId' | tr -s '\t' '\n') ]]
-#         then
-#                 AMIDELTAG="$instance_id"_"$(date +%d%b%y --date ''$retention_days' days ago')"
-
-#                 #Finding Image ID of instance which needed to be Deregistered
-#                 AMIDELETE=$(aws ec2 describe-images --filters Name=description,Values="$AMIDELTAG" --query 'Images[*].ImageId' | tr -s '\t' '\n')
-
-#                 #Find the snapshots attached to the Image need to be Deregister
-#                 aws ec2 describe-images --filters Name=image-id,Values="$AMIDELETE" --query 'Images[*].BlockDeviceMappings[*].Ebs.SnapshotId' | tr -s '\t' '\n' > /tmp/snap.txt
-
-#                 #Deregistering the AMI
-#                 aws ec2 deregister-image --image-id "$AMIDELETE"
-
-#                 #Deleting snapshots attached to AMI
-#                 while read SNAP_DEL; do
-#                         aws ec2 delete-snapshot --snapshot-id "$SNAP_DEL"
-#                 done < /tmp/snap.txt
-#         else
-#                 echo "No AMI present"
-#         fi
-# }
 
 ## SCRIPT COMMANDS ##
 
